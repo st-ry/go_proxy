8c40ffbebe81a98f550b2050e15e1a66445fed1d
st-ry <stry.makorepe@gmail.com>
2024-07-15 22:06:13 +0900

    add: serve

--- src/proxy/proxy.go
+++ src/proxy/proxy.go
@@ -2,11 +2,15 @@ package proxy
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func ServeProxy() {
 	http.HandleFunc("/", rootHandler)
+	if err := http.ListenAndServe(":18888", nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
